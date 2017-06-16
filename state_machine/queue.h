006f16991d533b60eca838acab88fbdeef695a30
Zvezdi Dimitrova <zvezdi.dim@gmail.com>
2017-06-16 15:16:36 +0300

    Commit the .h file in full as well

--- state_machine/queue.h
+++ state_machine/queue.h
@@ -6,7 +6,13 @@ typedef struct {
   STATE_ID* data;
   int size;
   int capacity;
+  int first;
+  int last;
 } queue_t;
 
 void push(queue_t* queue, STATE_ID state);
 STATE_ID pop(queue_t* queue);
+
+//private
+void resize(queue_t* queue);
+bool empty(queue_t* queue);
